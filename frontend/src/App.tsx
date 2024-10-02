fddbe701f6e467bbe0ef41842722f7b297a730e5
Crassula1994 <gogun3535@gmail.com>
2024-10-02 18:24:50 +0900

    Fix: Remove unnecessary files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,9 +15,7 @@ export default function App() {
     return (
         <AppContainer>
             <GlobalStyle />
-            <div>
-                <AppRoutes />
-            </div>
+            <AppRoutes />
         </AppContainer>
     );
 }
