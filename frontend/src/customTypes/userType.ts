5891906faa647c91def894b0cd1c06102000301e
Crassula1994 <gogun3535@gmail.com>
2024-10-02 18:34:25 +0900

    Fix: Modify user type interface

--- frontend/src/customTypes/userType.ts
+++ frontend/src/customTypes/userType.ts
@@ -1,5 +1,13 @@
 export interface UserType {
+    userId: string;
     username: string;
     email: string;
     password: string;
+    signInLog?: [
+        {
+            date: string;
+            type: string;
+            address: string;
+        }
+    ];
 }
